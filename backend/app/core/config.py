e79901747646ef69d390f4221dbfb9cb7b06a6b6
makdinoven <makdinoven@mail.ru>
2025-01-29 15:44:49 +0300

    edit backend config

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -3,18 +3,17 @@ import os
 from pydantic import BaseSettings
 
 class Settings(BaseSettings):
-    DB_USER: str = "root"
-    DB_PASSWORD: str = "root"
-    DB_HOST: str = "localhost"
+    DB_USER: str = "admin"
+    DB_PASSWORD: str = "mysqlpassword"
+    DB_HOST: str = "mysql"
     DB_PORT: str = "3306"
-    DB_NAME: str = "mydatabase"
+    DB_NAME: str = "dentsdatabase"
 
     SECRET_KEY: str = "CHANGE_ME"
     ALGORITHM: str = "HS256"
     ACCESS_TOKEN_EXPIRE_MINUTES: int = 60
 
     class Config:
-        # Можно указать env_file=".env", если вы используете .env
         env_file = None
 
 
