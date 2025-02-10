17d6b229861cef9ed687bd711629daffbb787ea6
makdinoven <119597764+makdinoven@users.noreply.github.com>
2025-02-11 02:00:52 +0300

    Update main.py

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,8 +1,9 @@
 from sys import prefix
-from fastapi import FastAPI, CORSMiddleware
+from fastapi import FastAPI
 from app.models.models import Base
 from app.db.database import engine
 from app.api import test, users, landings, authors, courses, payments
+from fastapi.middleware.cors import CORSMiddleware
 
 def create_app() -> FastAPI:
     app = FastAPI()
