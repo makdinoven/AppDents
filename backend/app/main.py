ba7d3fa2def61982ad40c77ea0459724eebae6a9
makdinoven <makdinoven@mail.ru>
2025-02-11 01:38:35 +0300

    Add CORS

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,11 +1,19 @@
 from sys import prefix
-from fastapi import FastAPI
+from fastapi import FastAPI, CORSMiddleware
 from app.models.models import Base
 from app.db.database import engine
 from app.api import test, users, landings, authors, courses, payments
 
 def create_app() -> FastAPI:
     app = FastAPI()
+
+    app.add_middleware(
+        CORSMiddleware,
+        allow_origins=["*"],
+        allow_credentials=True,
+        allow_methods=["*"],
+        allow_headers=["*"],
+    )
 
     # Подключаем роуты
     app.include_router(test.router, prefix="")
