92d56091bef97b84c5c10e181fb0e2f52c3e8f6f
makdinoven <makdinoven@mail.ru>
2025-02-17 13:24:10 +0300

    add swagger to prod

--- backend/app/main.py
+++ backend/app/main.py
@@ -2,7 +2,7 @@ from sys import prefix
 from fastapi import FastAPI
 from app.models.models import Base
 from app.db.database import engine
-from app.api import test, users, landings, authors, courses, payments
+from app.api import test, users, landings, authors, courses, payments, parser
 from fastapi.middleware.cors import CORSMiddleware
 from .db.database import init_db
 
@@ -25,6 +25,7 @@ def create_app() -> FastAPI:
     app.include_router(authors.router, prefix="/authors", tags=["Authors"])
     app.include_router(courses.router, prefix="/courses", tags=["Courses"])
     app.include_router(payments.router, prefix="/payments", tags=["Payments"])
+    app.include_router(parser.router, prefix="/parser", tags=["Parser"])
 
     @app.on_event("startup")
     async def startup_event():
