7e9dd6c328c584aedac8bac21ca9c67b7d0b974e
makdinoven <makdinoven@mail.ru>
2025-02-26 22:59:40 +0300

    fix

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI
-from app.api import test, users, landings, authors, courses, payments
+from app.api import test, users, landings, authors, courses, payments, parser
 from fastapi.middleware.cors import CORSMiddleware
 from .db.database import init_db
 
@@ -28,12 +28,13 @@ def create_app() -> FastAPI:
     )
 
     # Подключаем роуты
-    app.include_router(test.router, prefix="")
+    app.include_router(test.router, prefix="/test")
     app.include_router(users.router, prefix="/users", tags=["Users"])
     app.include_router(landings.router, prefix="/landings", tags=["Landing"])
     app.include_router(authors.router, prefix="/authors", tags=["Authors"])
     app.include_router(courses.router, prefix="/courses", tags=["Courses"])
     app.include_router(payments.router, prefix="/payments", tags=["Payments"])
+    app.include_router(parser.router, prefix="/parser", tags=["Parser"])
 
     @app.on_event("startup")
     async def startup_event():
