c8d4b114a2a7d2af836ec2808014f402aa7c9280
Alexey <galygin99@gmail.com>
2025-03-06 20:38:45 +0300

    started workin on landing page + multiple tag select

--- frontend/src/common/helpers/helpers.ts
+++ frontend/src/common/helpers/helpers.ts
@@ -11,3 +11,68 @@ export const getAuthHeaders = () => {
 };
 
 export const generateId = () => Math.floor(Math.random() * 100000);
+
+export const normalizeCourse = (course: any) => {
+  return {
+    ...course,
+    sections: course.sections.map((sectionObj: any) => {
+      const sectionId = Object.keys(sectionObj)[0];
+      return {
+        id: Number(sectionId),
+        section_name: sectionObj[sectionId].section_name,
+        lessons: sectionObj[sectionId].lessons.map(
+          (lesson: any, index: number) => ({
+            id: index + 1,
+            lesson_name: lesson.lesson_name,
+            video_link: lesson.video_link,
+          }),
+        ),
+      };
+    }),
+  };
+};
+
+export const denormalizeCourse = (course: any) => {
+  return {
+    ...course,
+    sections: course.sections.map((section: any) => ({
+      [section.id]: {
+        section_name: section.section_name,
+        lessons: section.lessons.map((lesson: any) => {
+          const { id, ...rest } = lesson;
+          return rest;
+        }),
+      },
+    })),
+  };
+};
+
+export const normalizeLessons = (lessons: any[]): any[] => {
+  return lessons.map((lessonObj, index) => {
+    const key = Object.keys(lessonObj)[0];
+    const lesson = lessonObj[key];
+
+    return {
+      id: index + 1,
+      program: lesson.program || "",
+      link: lesson.link || "",
+      duration: lesson.duration || "",
+      name: lesson.name || "",
+      lecturer: lesson.lecturer || "",
+    };
+  });
+};
+
+export const denormalizeLessons = (lessons: any) => {
+  return lessons.map((lesson: any, index: number) => {
+    return {
+      [`lesson${index + 1}`]: {
+        link: lesson.link || "",
+        name: lesson.name || "",
+        program: lesson.program || "",
+        duration: lesson.duration || "",
+        lecturer: lesson.lecturer || "",
+      },
+    };
+  });
+};
--- frontend/src/pages/Admin/modules/common/PanelItem/PanelItem.tsx
+++ frontend/src/pages/Admin/modules/common/PanelItem/PanelItem.tsx
@@ -6,18 +6,34 @@ import { t } from "i18next";
 interface PanelItemType {
   name: string;
   link: string;
+  landingPath?: string;
   id?: number;
 }
 
-const PanelItem = ({ link, name, id }: PanelItemType) => {
+const PanelItem = ({ link, name, id, landingPath }: PanelItemType) => {
+  const renderButtons = () => {
+    return landingPath ? (
+      <div className={s.buttons}>
+        <Link to={link}>
+          <PrettyButton text={t("update")} />
+        </Link>
+        <Link to={landingPath}>
+          <PrettyButton variant={"primary"} text={t("view")} />
+        </Link>
+      </div>
+    ) : (
+      <Link to={link}>
+        <PrettyButton text={t("update")} />
+      </Link>
+    );
+  };
+
   return (
     <div className={s.item}>
       <div>
         {id && <span>{id}</span>} {name}
       </div>
-      <Link to={link}>
-        <PrettyButton text={t("update")} />
-      </Link>
+      {renderButtons()}
     </div>
   );
 };
