f865353c9aaee7ce32bed4516957065f70bca29c
makdinoven <makdinoven@mail.ru>
2025-03-18 19:30:19 +0300

    New CI/CD and first v payment

--- backend/app/dependencies/auth.py
+++ backend/app/dependencies/auth.py
@@ -1,3 +1,5 @@
+from typing import Optional
+
 from fastapi import HTTPException, Depends
 from fastapi.security import OAuth2PasswordBearer
 from jose import jwt, JWTError
@@ -9,7 +11,7 @@ from ..core.config import settings
 from ..models.models_v2 import User
 
 # Добавляем схему для OAuth2
-oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/users/login")
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/users/login",auto_error=False)
 
 
 from fastapi import Depends, HTTPException
@@ -24,22 +26,26 @@ def get_current_user(token: str = Depends(oauth2_scheme), db: Session = Depends(
         raise HTTPException(status_code=401, detail="User not found")
     return user
 
-def get_current_user_optional(db: Session = Depends(get_db), token: str = Depends(oauth2_scheme,
-                                    default=None)) -> User | None:
+
+def get_current_user_optional(
+        db: Session = Depends(get_db),
+        token: str = Depends(oauth2_scheme)
+) -> Optional[User]:
     """
     Возвращает текущего пользователя, если токен передан и валиден,
     или None, если токена нет/некорректен.
-    Не кидает исключение 401.
+    Не выбрасывает 401 Unauthorized.
     """
     if not token:
-        return None
+        return None  # Токен не передан
+
     try:
         payload = jwt.decode(token, settings.SECRET_KEY, algorithms=[settings.ALGORITHM])
         user_id: int = payload.get("user_id")
         if user_id is None:
             return None
     except JWTError:
-        return None
+        return None  # Ошибка при декодировании/проверке токена
 
     user = db.query(User).filter(User.id == user_id).first()
     return user
