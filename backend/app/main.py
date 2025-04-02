12d821650bb594685db2670ecc15e985e176bd0b
makdinoven <makdinoven@mail.ru>
2025-04-03 02:04:18 +0300

    fix name foe schemas

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,6 +1,5 @@
-from sys import prefix
+
 from fastapi import FastAPI
-# from app.api import test, users, landings, authors, courses, payments, parser, cleaner, transfer
 from .api_v2 import cleaner, users, courses, landings, authors, photo, stripe
 from fastapi.middleware.cors import CORSMiddleware
 from .db.database import init_db
