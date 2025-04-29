06fb9d29f3597d140c215d610a85b4a0d89307c6
Alexey <galygin99@gmail.com>
2025-04-29 10:14:40 +0300

    pagination working

--- frontend/src/components/ui/Pagination/PaginationButton/PaginationButton.tsx
+++ frontend/src/components/ui/Pagination/PaginationButton/PaginationButton.tsx
@@ -1,7 +1,22 @@
 import s from "./PaginationButton.module.scss";
 
-const PaginationButton = () => {
-  return <div className={s.button}></div>;
+const PaginationButton = ({
+  onClick,
+  pageNumber,
+  activePage,
+}: {
+  onClick: () => void;
+  pageNumber: number;
+  activePage: number;
+}) => {
+  return (
+    <button
+      onClick={onClick}
+      className={activePage === pageNumber ? s.activePage : ""}
+    >
+      {pageNumber}
+    </button>
+  );
 };
 
 export default PaginationButton;
