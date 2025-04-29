6783e4f8de03a712b8732b01d8d4d8e3b96672fb
Alexey <galygin99@gmail.com>
2025-04-29 10:43:33 +0300

    working warn component + warn localization

--- frontend/src/components/Header/Header.tsx
+++ frontend/src/components/Header/Header.tsx
@@ -95,26 +95,6 @@ const Header = () => {
 
   return (
     <>
-      {/*<div className={s.working_warn}>*/}
-      {/*  <h2>*/}
-      {/*    <Trans i18nKey={"workingWarn.title"} />*/}
-      {/*  </h2>*/}
-      {/*  <p>*/}
-      {/*    <Trans i18nKey={"workingWarn.first"} />*/}
-      {/*  </p>*/}
-      {/*  <p>*/}
-      {/*    <Trans i18nKey={"workingWarn.second"} />*/}
-      {/*  </p>*/}
-      {/*  <p>*/}
-      {/*    <Trans i18nKey={"workingWarn.third"} />*/}
-      {/*  </p>*/}
-      {/*  <p>*/}
-      {/*    <Trans i18nKey={"workingWarn.fourth"} />*/}
-      {/*  </p>*/}
-      {/*  <p>*/}
-      {/*    <Trans i18nKey={"workingWarn.last"} />*/}
-      {/*  </p>*/}
-      {/*</div>*/}
       <header className={s.header}>
         <div className={s.content}>
           <Link className={s.logo} to={Path.main}>
