22e0fc8ff9474e207b96872700ebd36aa516a6c1
Alexey <galygin99@gmail.com>
2025-05-21 18:11:48 +0300

    landing ad

--- frontend/src/pages/ProfessorPage/ProfessorPage.tsx
+++ frontend/src/pages/ProfessorPage/ProfessorPage.tsx
@@ -151,6 +151,7 @@ const ProfessorPage = () => {
           <div className={s.professor_cards}>
             <SectionHeader name={"professor.professorsCourses"} />
             <CardsList
+              // isClient={true}
               filter={"all"}
               loading={false}
               cards={professor.landings}
