14e1528d7da5ab25a71d1c647e9a3465e4a1081b
Alexey <galygin99@gmail.com>
2025-06-04 21:16:46 +0300

    analytics purchases page

--- frontend/src/pages/Admin/tabs/Analytics/Analytics.tsx
+++ frontend/src/pages/Admin/tabs/Analytics/Analytics.tsx
@@ -4,6 +4,7 @@ import AnalyticsLanguages from "./content/AnalyticsLanguages.tsx";
 import AnalyticsListing from "./content/AnalyticsListing.tsx";
 import AnalyticsReferrals from "./content/AnalyticsReferrals.tsx";
 import { useSearchParams } from "react-router-dom";
+import AnalyticsPurchases from "./content/AnalyticsPurchases.tsx";
 
 const QUERY_KEY = "content";
 
@@ -15,25 +16,32 @@ const Analytics = () => {
     {
       name: "admin.analytics.landings",
       value: "landings",
-      component: <AnalyticsLanguages />,
+      component: (title: string) => <AnalyticsLanguages title={title} />,
     },
     {
       name: "admin.analytics.listing",
       value: "listing",
-      component: <AnalyticsListing />,
+      component: (title: string) => <AnalyticsListing title={title} />,
     },
     {
       name: "admin.analytics.referrals",
       value: "referrals",
-      component: <AnalyticsReferrals />,
+      component: (title: string) => <AnalyticsReferrals title={title} />,
+    },
+    {
+      name: "admin.analytics.purchases",
+      value: "purchases",
+      component: (title: string) => <AnalyticsPurchases title={title} />,
     },
   ];
+
+  const activeTab = analyticsContent.find((tab) => tab.value === tabFromParams);
 
   return (
     <div className={s.analytics_page}>
       <Tabs queryKey={QUERY_KEY} mainTab={"landings"} tabs={analyticsContent} />
 
-      {analyticsContent.find((tab) => tab.value === tabFromParams)?.component}
+      {activeTab?.component(activeTab.name)}
     </div>
   );
 };
---
+++ frontend/src/pages/Admin/tabs/Analytics/content/AnalyticsPurchases.tsx
@@ -0,0 +1,98 @@
+import { useEffect, useState } from "react";
+import { getFormattedDate } from "../../../../../common/helpers/helpers.ts";
+import s from "../Analytics.module.scss";
+import DateRangeFilter from "../../../../../components/ui/DateRangeFilter/DateRangeFilter.tsx";
+import { Trans } from "react-i18next";
+import { adminApi } from "../../../../../api/adminApi/adminApi.ts";
+import Loader from "../../../../../components/ui/Loader/Loader.tsx";
+import Table from "../../../../../components/ui/Table/Table.tsx";
+
+const AnalyticsPurchases = ({ title }: { title: string }) => {
+  const [data, setData] = useState<any>(null);
+  const [loading, setLoading] = useState(true);
+  const [dateRange, setDateRange] = useState(() => ({
+    startDate: getFormattedDate(new Date()),
+    endDate: getFormattedDate(new Date()),
+  }));
+
+  const handleStartDateChange = (value: string) => {
+    setDateRange((prev) => ({ ...prev, startDate: value }));
+  };
+
+  const handleEndDateChange = (value: string) => {
+    setDateRange((prev) => ({ ...prev, endDate: value }));
+  };
+
+  const fetchData = async () => {
+    setLoading(true);
+    const params = {
+      start_date: dateRange.startDate,
+      end_date: dateRange.endDate,
+    };
+
+    try {
+      const res = await adminApi.getPurchases(params);
+      setData({
+        items: res.data.items,
+        total: res.data.total,
+        total_amount: res.data.total_amount,
+      });
+      setLoading(false);
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, [dateRange]);
+
+  useEffect(() => {
+    console.log(data);
+  }, [data]);
+
+  return (
+    <>
+      <h2>
+        <Trans i18nKey={title} />
+      </h2>
+      <div className={s.analytics_options}>
+        <DateRangeFilter
+          startDate={dateRange.startDate}
+          endDate={dateRange.endDate}
+          onEndDateChange={handleEndDateChange}
+          onStartDateChange={handleStartDateChange}
+        />
+        {data && (
+          <>
+            <p>
+              Total:
+              <span className={"highlight_blue_bold"}>{data.total}</span>
+            </p>
+            <p>
+              Amount:
+              <span className={"highlight_blue_bold"}>{data.total_amount}</span>
+            </p>
+          </>
+        )}
+      </div>
+
+      {!data && loading ? (
+        <Loader />
+      ) : (
+        <Table
+          data={data.items}
+          columnLabels={{
+            email: "Email",
+            amount: "Amount",
+            source: "Source",
+            from_ad: "Ad",
+            paid_at: "Payment date",
+          }}
+        />
+      )}
+    </>
+  );
+};
+
+export default AnalyticsPurchases;
