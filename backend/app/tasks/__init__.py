77441281be86e91cfdcabece564ea9298702dcd4
makdinoven <makdinoven@mail.ru>
2025-06-24 15:21:02 +0300

    Fix worker

--- backend/app/tasks/__init__.py
+++ backend/app/tasks/__init__.py
@@ -1,2 +1,4 @@
 from .preview_tasks import generate_preview      # noqa: F401
 from .special_offers import process_special_offers  # noqa: F401
+from .fast_start       import ensure_faststart, process_faststart_video
+from .storage_links    import replace_storage_links
