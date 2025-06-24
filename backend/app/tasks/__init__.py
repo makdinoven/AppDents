f7e1be7520eadebb556d18edab0ba8c09ff27f23
makdinoven <makdinoven@mail.ru>
2025-06-24 15:43:03 +0300

    Fix worker

--- backend/app/tasks/__init__.py
+++ backend/app/tasks/__init__.py
@@ -2,3 +2,11 @@ from .preview_tasks import generate_preview      # noqa: F401
 from .special_offers import process_special_offers  # noqa: F401
 from .fast_start       import ensure_faststart, process_faststart_video
 from .storage_links    import replace_storage_links
+
+__all__ = [
+    "ensure_faststart",
+    "process_faststart_video",
+    "generate_preview",
+    "process_special_offers",
+    "replace_storage_links",
+]
