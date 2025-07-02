dbe9ff504a95c6bc0bf8b4c319017605673601b0
solovikas <solovvika2005@gmail.com>
2025-07-02 23:37:46 +0300

    Refs-trans saving changes

--- frontend/src/components/ui/Alert/Alert.tsx
+++ frontend/src/components/ui/Alert/Alert.tsx
@@ -7,7 +7,7 @@ export const Alert = (title: string, icon?: ReactElement) => {
   withReactContent(Swal).fire({
     title: (
       <div className="custom_swal_title_container">
-        {icon && <div className="alert_circle">{icon}</div>}
+        {icon && <div className="alert_icon">{icon}</div>}
         {title}
       </div>
     ),
