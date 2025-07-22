96ff9b0eb3d53c48e9d0750bde6e539d04d69cc9
makdinoven <makdinoven@mail.ru>
2025-07-22 14:47:50 +0300

    update url replace task

--- backend/app/tasks/storage_links.py
+++ backend/app/tasks/storage_links.py
@@ -3,7 +3,7 @@ import sqlalchemy
 from sqlalchemy import text
 from celery import shared_task
 
-from ...db.database import SessionLocal  # скорректируйте путь под вашу структуру
+from ..db.database import SessionLocal  # скорректируйте путь под вашу структуру
 
 logger = logging.getLogger(__name__)
 
