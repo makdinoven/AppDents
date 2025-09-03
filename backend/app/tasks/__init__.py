3ada9d4f5f834dd455f26f6c07da222c6a40e1bb
Maksim <makdinoven@mail.ru>
2025-09-03 15:07:14 +0300

    fix generator

--- backend/app/tasks/__init__.py
+++ backend/app/tasks/__init__.py
@@ -2,6 +2,8 @@ from .preview_tasks import generate_preview      # noqa: F401
 from .special_offers import process_special_offers  # noqa: F401
 from .fast_start       import ensure_faststart, process_faststart_video
 from .storage_links    import replace_storage_links
+from .book_previews import generate_book_preview
+from .book_formats import generate_book_formats
 
 __all__ = [
     "ensure_faststart",
@@ -9,4 +11,6 @@ __all__ = [
     "generate_preview",
     "process_special_offers",
     "replace_storage_links",
+    "generate_book_preview",
+    "generate_book_formats",
 ]
