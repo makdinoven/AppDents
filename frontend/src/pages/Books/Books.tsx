3ebfbb76075f199680dd9e08961d7650ba41a9fa
Alexey <galygin99@gmail.com>
2025-09-07 18:58:19 +0300

    error fix

--- frontend/src/pages/Books/Books.tsx
+++ frontend/src/pages/Books/Books.tsx
@@ -1,16 +1,11 @@
 import s from "./Books.module.scss";
 import { useDispatch, useSelector } from "react-redux";
 import { AppDispatchType, AppRootStateType } from "../../store/store.ts";
-import { useEffect, useState } from "react";
-import { ParamsType } from "../../api/adminApi/types.ts";
+import { useEffect } from "react";
 import DetailHeader from "../Admin/modules/common/DetailHeader/DetailHeader.tsx";
-import ListController from "../../components/ui/ListController/ListController.tsx";
-import CardsList from "../../components/CommonComponents/CoursesSection/CardsList/CardsList.tsx";
-import { mainApi } from "../../api/mainApi/mainApi.ts";
 import { useNavigate } from "react-router-dom";
 import { Path } from "../../routes/routes.ts";
 import { getCourses } from "../../store/actions/userActions.ts";
-import CoursesSection from "../../components/CommonComponents/CoursesSection/CoursesSection.tsx";
 
 const Books = () => {
   const navigate = useNavigate();
@@ -21,11 +16,11 @@ const Books = () => {
   const userCourses = useSelector(
     (state: AppRootStateType) => state.user.courses,
   );
-  const [courses, setCourses] = useState([]);
-  const [total, setTotal] = useState(0);
-  const [totalPages, setTotalPages] = useState(0);
-  const [loading, setLoading] = useState(false);
-  const [isFirstLoad, setIsFirstLoad] = useState(true);
+  // const [courses, setCourses] = useState([]);
+  // const [total, setTotal] = useState(0);
+  // const [totalPages, setTotalPages] = useState(0);
+  // const [loading, setLoading] = useState(false);
+  // const [isFirstLoad, setIsFirstLoad] = useState(true);
   const isAdmin = role === "admin";
 
   useEffect(() => {
@@ -40,53 +35,53 @@ const Books = () => {
     }
   }, [userCourses]);
 
-  const loadCourses = async (params: ParamsType) => {
-    setLoading(true);
-    try {
-      const paramsToSend = { ...params };
-      const res = await mainApi.getLandingCards(paramsToSend);
-
-      setCourses(res.data.cards);
-      setTotal(res.data.total);
-      setTotalPages(res.data.total_pages);
-      setIsFirstLoad(false);
-    } catch (error) {
-      console.log(error);
-    } finally {
-      setLoading(false);
-    }
-  };
+  // const loadCourses = async (params: ParamsType) => {
+  //   setLoading(true);
+  //   try {
+  //     const paramsToSend = { ...params };
+  //     const res = await mainApi.getLandingCards(paramsToSend);
+  //
+  //     setCourses(res.data.cards);
+  //     setTotal(res.data.total);
+  //     setTotalPages(res.data.total_pages);
+  //     setIsFirstLoad(false);
+  //   } catch (error) {
+  //     console.log(error);
+  //   } finally {
+  //     setLoading(false);
+  //   }
+  // };
 
   return (
     <div lang={language.toLowerCase()} className={s.courses}>
       <DetailHeader title={"books.title"} />
-      <ListController
-        language={language}
-        size={10}
-        type="books"
-        loadData={(params) => loadCourses(params)}
-        total={total}
-        totalPages={totalPages}
-        filters={["tags", "sort", "size"]}
-      >
-        <CardsList
-          isBook={true}
-          isClient={true}
-          loading={loading}
-          showSeeMore={false}
-          showEndOfList={false}
-          cards={courses}
-        />
-      </ListController>
-      {!isFirstLoad && (
-        <CoursesSection
-          isBook={true}
-          isOffer={true}
-          showSort={true}
-          sectionTitle={"other.otherBooks"}
-          pageSize={4}
-        />
-      )}
+      {/*<ListController*/}
+      {/*  language={language}*/}
+      {/*  size={10}*/}
+      {/*  type="books"*/}
+      {/*  loadData={(params) => loadCourses(params)}*/}
+      {/*  total={total}*/}
+      {/*  totalPages={totalPages}*/}
+      {/*  filters={["tags", "sort", "size"]}*/}
+      {/*>*/}
+      {/*<CardsList*/}
+      {/*  isBook={true}*/}
+      {/*  isClient={true}*/}
+      {/*  loading={loading}*/}
+      {/*  showSeeMore={false}*/}
+      {/*  showEndOfList={false}*/}
+      {/*  cards={courses}*/}
+      {/*/>*/}
+      {/*</ListController>*/}
+      {/*{!isFirstLoad && (*/}
+      {/*  <CoursesSection*/}
+      {/*    isBook={true}*/}
+      {/*    isOffer={true}*/}
+      {/*    showSort={true}*/}
+      {/*    sectionTitle={"other.otherBooks"}*/}
+      {/*    pageSize={4}*/}
+      {/*  />*/}
+      {/*)}*/}
     </div>
   );
 };
