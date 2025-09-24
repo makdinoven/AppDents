f1fc6de5f18bc8bcd71b27e9d62c1a3a588f648f
Alexey <galygin99@gmail.com>
2025-09-24 14:26:27 +0300

    different color for ad visits in landing analytics chart

--- frontend/src/pages/Admin/tabs/Analytics/Charts/LandingAnalyticsChart.tsx
+++ frontend/src/pages/Admin/tabs/Analytics/Charts/LandingAnalyticsChart.tsx
@@ -14,6 +14,7 @@ import LoaderOverlay from "../../../../../components/ui/LoaderOverlay/LoaderOver
 
 const lineColors: Record<string, string> = {
   visits: "#01433d",
+  ad_visits: "#006d8d",
   purchases: "#90e895",
 };
 
@@ -31,7 +32,7 @@ const CustomTooltip = ({ active, payload }: any) => {
       <p className={s.tooltip_date}>{fullDate}</p>
       {payload.map((entry: any) => (
         <p className={s[entry.name]} key={entry.dataKey}>
-          {entry.name}: {entry.value}
+          {entry.name === "ad_visits" ? "Ad visits" : entry.name}: {entry.value}
         </p>
       ))}
     </div>
