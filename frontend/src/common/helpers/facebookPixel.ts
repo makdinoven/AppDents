d39533d2843754920e98cad0c62248f7ec4e0322
Alexey <galygin99@gmail.com>
2025-09-26 11:27:29 +0300

    new pixel

--- frontend/src/common/helpers/facebookPixel.ts
+++ frontend/src/common/helpers/facebookPixel.ts
@@ -5,7 +5,12 @@ const options = {
   debug: false,
 };
 
-const pixelIds = ["355039504358349", "1105921817166633", "975797817469515"];
+const pixelIds = [
+  "355039504358349",
+  "1105921817166633",
+  "975797817469515",
+  "726394920443618",
+];
 
 const languagePixelIds: Record<string, string> = {
   IT: "722350576947991",
