869f42993b004bbfc3b777011e13531bba3b0b5e
solovikas <solovvika2005@gmail.com>
2025-10-18 23:26:46 +0300

    Added download fuction and proper reader render

--- frontend/src/components/CommonComponents/BuySection/BuySection.tsx
+++ frontend/src/components/CommonComponents/BuySection/BuySection.tsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import s from "./BuySection.module.scss";
 import { Trans, useTranslation } from "react-i18next";
 import { ToothPatch } from "../../../assets";
+import { Alert } from "../../ui/Alert/Alert.tsx";
 
 interface BuySectionProps {
   type: "download" | "buy";
@@ -9,8 +10,8 @@ interface BuySectionProps {
   isFullWidth?: boolean;
   oldPrice?: string;
   newPrice?: string;
-  provideUrl: (format: string) => string;
-  openPayment: () => void;
+  downloadInfo?: (format: string) => { url: string; name: string };
+  openPayment?: () => void;
 }
 
 const BuySection: React.FC<BuySectionProps> = ({
@@ -19,7 +20,7 @@ const BuySection: React.FC<BuySectionProps> = ({
   isFullWidth = false,
   oldPrice,
   newPrice,
-  provideUrl,
+  downloadInfo,
   openPayment,
 }: BuySectionProps) => {
   const [activeFormat, setActiveFormat] = useState(formats?.[0] || "PDF");
@@ -32,6 +33,36 @@ const BuySection: React.FC<BuySectionProps> = ({
   const handleFormatChange = (format: string) => {
     setActiveFormat(format);
   };
+
+  const handleDownload = async (format: string) => {
+    if (!downloadInfo) {
+      return;
+    }
+    const { url, name } = downloadInfo(format);
+
+    if (!url || url === "#") {
+      return;
+    }
+
+    try {
+      const file = await fetch(url);
+      const blob = await file.blob();
+
+      const link = document.createElement("a");
+      link.href = window.URL.createObjectURL(blob);
+      link.download = `${name}.${format.toLowerCase()}`;
+      document.body.appendChild(link);
+      link.click();
+      document.body.removeChild(link);
+
+      window.URL.revokeObjectURL(link.href);
+    } catch (error) {
+      console.error("Download error: ", error);
+      Alert(t("downloadError"));
+    }
+  };
+
+  const onClick = isBuy ? openPayment : () => handleDownload(activeFormat);
 
   return (
     <div className={s.section_wrapper}>
@@ -53,21 +84,19 @@ const BuySection: React.FC<BuySectionProps> = ({
             components={[<span className={s.highlight} />]}
           />
         </p>
-        {isBuy ? (
-          <button onClick={openPayment} className={`${s.buy_button} ${s[buy]}`}>
-            <p className={s.prices}>
-              {t("buyFor")} <span className={s.old_price}>${oldPrice}</span>
-              <span className={s.new_price}>${newPrice}</span>
-            </p>
-          </button>
-        ) : (
-          <a href={provideUrl(activeFormat)} className={s.buy_button}>
+        <button onClick={onClick} className={`${s.buy_button} ${s[buy]}`}>
+          {isDownload ? (
             <p>
               {t("bookLanding.download")}
               <span style={{ marginLeft: "10px" }}>{activeFormat}</span>
             </p>
-          </a>
-        )}
+          ) : (
+            <p className={s.prices}>
+              {t("buyFor")} <span className={s.old_price}>${oldPrice}</span>
+              <span className={s.new_price}>${newPrice}</span>
+            </p>
+          )}
+        </button>
         {isDownload && (
           <ul className={s.format_buttons}>
             {formats?.map((format: string) => {
