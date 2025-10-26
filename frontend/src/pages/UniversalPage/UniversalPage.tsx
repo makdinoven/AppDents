3e2d3de9296ad8fe244830d5970fca700e901144
Виктория Соловьёва <solovikas@Mac.lan>
2025-10-26 19:51:11 +0300

    Added policies text fetch

--- frontend/src/pages/UniversalPage/UniversalPage.tsx
+++ frontend/src/pages/UniversalPage/UniversalPage.tsx
@@ -1,25 +1,44 @@
 import s from "./UniversalPage.module.scss";
 import { useParams } from "react-router-dom";
 import DetailHeader from "../Admin/pages/modules/common/DetailHeader/DetailHeader.tsx";
-import { t } from "i18next";
+import { useTranslation } from "react-i18next";
+import { useEffect, useState } from "react";
+import { mainApi } from "../../api/mainApi/mainApi.ts";
 
 const UniversalPage = () => {
   const { pageType } = useParams();
+  const { i18n } = useTranslation();
+  const language = i18n.language.toLowerCase();
+  const [content, setContent] = useState<string>("");
 
   const pageInfo: Record<string, { title: string; content: string }> = {
     "terms-of-use": {
       title: "footer.terms",
-      content: t("footer.terms.content"),
+      content,
     },
     "privacy-policy": {
       title: "footer.privacy",
-      content: t("footer.privacy.content"),
+      content,
     },
     "cookie-policy": {
       title: "footer.cookie",
-      content: t("footer.cookie.content"),
+      content,
     },
   };
+
+  useEffect(() => {
+    if (!pageType || !language) return;
+    mainApi
+      .getPageInfo(pageType, language)
+      .then((res) => {
+        if (res.data) {
+          setContent(res.data.content || "");
+        }
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, [language, pageType]);
 
   const page = pageInfo[pageType || ""] || { title: "", content: "" };
   const splitBySentences = (text: string) => {
